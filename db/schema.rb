906bb6677b8467c1e53c8654a23375876f592957
Sanoryel <lucas.leyronas@orange.fr>
2024-03-24 22:05:03 +0100

    Add username

--- db/schema.rb
+++ db/schema.rb
@@ -19,8 +19,10 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_24_194834) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
