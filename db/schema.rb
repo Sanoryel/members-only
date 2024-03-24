21f7428dfd286b070072a8c2eaa2cffa6b0dac4e
Sanoryel <lucas.leyronas@orange.fr>
2024-03-24 22:06:51 +0100

    Add post

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,15 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_03_24_194834) do
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -25,4 +34,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_24_194834) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
